d2db8532f32dbe2f85297d2790b09f6820b9b0a5
Vítor <160982232+VitorSilvaDosSantos@users.noreply.github.com>
2024-05-02 21:39:39 -0300

    Segundo Bimestre 21:39

--- 10-API-MongoDB/index.js
+++ 10-API-MongoDB/index.js
@@ -4,16 +4,53 @@ const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
+//Middleware
 app.use(express.json())
 
+//DB Modelos
+const Tarefa = mongoose.model('tarefa', { nome: String })
+
+//Rotas
 app.get('/hello', (req, res) => {
     res.json("Hello")
+})
+
+app.get('/tarefas', async (req, res) => {
+    const tarefas = await Tarefa.find()
+    res.json(tarefa)
+})
+
+//Att
+
+app.put('/tarefas/:id', async (req, res) => {
+    const tarefa = await Tarefa.findByIdAndUpdate(req.params.id, { nome: req.body.nome }, { new: true})
+    res.json(tarefa)
+})
+
+app.get('/tarefas/:id', async (req, res) => {
+    const tarefas = await Tarefa.findById(req.params.id)
+    res.json(tarefa)
+})
+
+app.post('/tarefas', async (req, res) => {
+    const corpo = req.body
+    const tarefa = new Tarefa({ nome: req.body.nome })
+    await tarefa.save()
+    res.json(tarefa)
+})
+
+//delete
+
+app.delete('/tarefas/:id', async (req, res) => {
+    await Tarefa.findByIdAndDelete(req.params.id)
+    req.jason()
 })
 
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@minhaapi.rn8e1vu.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://vitorsantos:R7MfS76bJhFqh2CY@minhaapi.tpvifqe.mongodb.net/?
+retryWrites=true&w=majority&appName=minhaAPI`)
     .then(() => console.log("Conectado ao meu MongoDB!"))
     .catch(err => console.log("Erro ao conectar no meu MongoDB: ", err))
 
